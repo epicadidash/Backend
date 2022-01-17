75d3c24e34fa7ad094328677f214b2e706dcabea
EpicAdidash <dradwaitdash@gmail.com>
2022-01-17 13:18:12 +0530

    Feat: Read Note Endpoint Complete          In this commit , the endpoint for reading note is complete. The api is based directly on the core packages and language itself. The mecwn below.
                -------- Mechanisim -------
                Create Note current mechanisim:-
                *First it filters all get requests and respond to it accordingly.
                *Then it sends the information required from the database.
                -------- End -------

--- routes/api/read.js
+++ routes/api/read.js
@@ -1,3 +1,35 @@
 const express = require(`express`);
 const router = express.Router();
+const database = require("../../database/database");
+var con = database.con; 
+router.get(`/`,(req,res)=>
+{
+    const id = req.query.id
+    if(!id)
+    {
+        res.status(400).send("Information Missing")
+    }
+    else 
+    {
+        con.query(
+            `SELECT ID, Title, Description, tag, Timestamp FROM notes WHERE UserID = '${id}';`,
+            function (err, result) {
+            if (err) 
+            {throw err;
+            }
+      else if(result.length>0)
+    {
+        res.send(result)
+    }
+    else if(result.length === 0)
+    {
+        res.json({warning:"no notes created by the user"})
+    }
+    else
+    {
+        res.status(500).send("Internal Server Error")
+    }
+})
+    }
+})
 module.exports = router;
