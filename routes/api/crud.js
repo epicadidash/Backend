2d7f18fe71ecad75dcff593f4b096808919b47ad
EpicAdidash <dradwaitdash@gmail.com>
2022-01-17 14:28:20 +0530

    Feat: API complete(Release of Backend 1.0.0)
    We are happy to announce the release of the first version of our backend .

--- routes/api/crud.js
+++ routes/api/crud.js
@@ -4,4 +4,31 @@ router.use(`/create`, require(`./create`));
 router.use(`/read`, require(`./read`));
 router.use(`/update`, require(`./update`));
 router.use(`/delete`, require(`./delete`));
+router.post(`/:anything`, (req, res) => {
+    res.status(404).send("Not Found");
+  });
+  router.get(`/:anything`, (req, res) => {
+    res.status(404).send("Not Found");
+  });
+  router.put(`/:anything`, (req, res) => {
+    res.status(404).send("Not Found");
+  });
+  router.delete(`/:anything`, (req, res) => {
+    res.status(404).send("Not Found");
+  });
+  router.patch(`/:anything`, (req, res) => {
+    res.status(404).send("Not Found");
+  });
+  router.head(`/:anything`, (req, res) => {
+    res.status(404).send("Not Found");
+  });
+  router.options(`/:anything`, (req, res) => {
+    res.status(404).send("Not Found");
+  });
+  router.trace(`/:anything`, (req, res) => {
+    res.status(404).send("Not Found");
+  });
+  router.connect(`/:anything`, (req, res) => {
+    res.status(404).send("Not Found");
+  });
 module.exports = router;
