067c249c7c2ed47ee145e0d744e680c45e5fe332
EpicAdidash <dradwaitdash@gmail.com>
2022-01-17 14:09:59 +0530

    Feat: Delete Note Endpoint Complete      In this commit , the endpoint for delete note is complete. The delete endpoint doesnot use any other api and is based directly on the core packages and language itself. The mechanisim for the devlopers is shown below.
            -------- Mechanisim -------
            Delete current mechanisim:-
            *First it filters all delete requests and respond to it accordingly.
            *Then it deletes the note of the id given.
            -------- End -------

--- routes/api/delete.js
+++ routes/api/delete.js
@@ -1,3 +1,25 @@
 const express = require(`express`);
 const router = express.Router();
+const database = require(`../../database/database`); 
+var con = database.con; 
+router.delete(`/`, (req, res) => {  
+  var parameter = req.query; 
+  var id = parameter.id;   
+  con.query(`SELECT * FROM notes WHERE ID = '${id}';`, function (err, result) {
+    if (err) throw err;
+    if (result.length === 0) {
+      res.status(400).send("No such user with this id found");
+    } else if (result.length === 1) {
+      con.query(    `DELETE FROM notes WHERE ID = '${id}';`,
+        function (err, result) {
+          if (err) throw err;
+          res.status(500).send("Internal Server Error");
+        }
+      );
+      res.send("Deleted Successfully");
+    } else {
+      res.status(500).send("Internal Server Error");
+    }
+  });
+});
 module.exports = router;
