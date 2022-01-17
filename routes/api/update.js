86fb0b40c2f061f866af4f1a737a122ac956447e
EpicAdidash <dradwaitdash@gmail.com>
2022-01-17 14:01:43 +0530

    Feat: update Note Endpoint Complete          In this commit , the endpoint for updatting note is complete. The  api  is based directly on the core packages and language itself. The mechanisim below.
                -------- Mechanisim -------
                Update Note current mechanisim:-
                *First it filters all put requests and respond to it accordingly.
                *Then it updates the information provided in the database.
                -------- End -------

--- routes/api/update.js
+++ routes/api/update.js
@@ -1,3 +1,41 @@
 const express = require(`express`);
 const router = express.Router();
+const database = require("../../database/database");
+var con = database.con;
+const date = new Date();
+let timestamp = date.toUTCString();
+router.put(`/`,(req,res)=>
+{
+    const body = req.body;
+    const title  = body.title;
+    const description = body.description;
+    const tag = body.tag;
+    const id = req.query.id
+    if(!title || !description || !id || !tag)
+    {
+        res.status(400).send("Information Missing")
+    }
+    else if(title.length <= 5)
+    {
+      res.status(400).send("Title length too short")
+    }
+    else if(description.length <= 10)
+    {
+      res.status(400).send("Description length too short")
+    }
+    else
+    {
+        con.query(
+            `UPDATE notes SET Title = '${title}', Description = '${description}', tag = '${tag}', Timestamp = '${timestamp}' WHERE ID = '${id}' ;`,
+            function (err, result) {
+              if (err) 
+              {
+                  throw err
+                res.status(500).send("Internal Server Error");
+              };
+              res.send("Note successfully updated")
+            }
+          );
+    }
+})
 module.exports = router;
