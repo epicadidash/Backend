c7b0e2098820cd935b87812512a4b692614754bb
EpicAdidash <dradwaitdash@gmail.com>
2022-01-27 23:41:52 +0530

    Feat: Test Deployment
        We are happy to announce the deployment of the first version .

--- database/database.js
+++ database/database.js
@@ -1,10 +1,10 @@
 var mysql = require("mysql"); //mysql package imported
 
 var connector = mysql.createConnection({ //mysql connector
-  host: "localhost",
-  user: "root",
-  password: "12345",
-  database: "note-plus",
+  host: "db4free.net",
+  user: "rootadwait",
+  password: "6XAPEU6ky6XDsdi",
+  database: "noteplus",
 });
 module.exports = { //export connector module
   con: connector,
